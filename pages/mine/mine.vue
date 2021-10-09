15aedd2f643de5abcab74f46c7e43e628e6c5c71
liushu <383883040@qq.com>
2021-10-09 22:31:11 +0800

    第一次提交

---
+++ pages/mine/mine.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view class="page-body">
+			2
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
