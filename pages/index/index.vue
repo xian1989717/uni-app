2744f3052c349f9af347ad986293e8570c39d6c3
liushu <383883040@qq.com>
2021-10-09 22:41:52 +0800

    提交代码

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,13 @@
 <template>
 	<view class="page-body">
-		1
+		<view style="display: flex;">
+			<view style="flex:1;">123</view>
+			<input class="uni-input" confirm-type="search" placeholder="键盘右下角按钮显示为搜索"
+				style="border:1px solid #007AFF;border-radius: 5px;flex:1;flex-grow: 5;" />
+			<view style="flex:1;">1234</view>
+			<view style="flex:1;">1234</view>
+		</view>
+
 	</view>
 </template>
 
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -142,6 +142,13 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
 var _default =
 {
   data: function data() {
